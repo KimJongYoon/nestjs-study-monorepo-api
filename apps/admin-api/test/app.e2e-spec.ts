5ed54faed0115d866d9b291e55b4cda2333ac367
mion <whddbs311@gmail.com>
2021-12-15 13:33:38 +0900

    e2e test 셋팅

--- apps/admin-api/test/app.e2e-spec.ts
+++ apps/admin-api/test/app.e2e-spec.ts
@@ -1,24 +1,92 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AdminApiModule } from './../src/admin-api.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import request from 'supertest';
+import { AdminApiModule } from '../src/admin/admin-api.module';
+import { MainModule } from '../src/main.module';
+import { before } from '@nestjs/swagger/dist/plugin';
+import {Admin} from "../src/admin/entities/admin.entity";
 
 describe('AdminApiController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AdminApiModule],
+      imports: [MainModule, AdminApiModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({ transform: true, whitelist: true }),
+    );
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('Admin Module', () => {
+    jest.setTimeout(60 * 1000); // 타임아웃 설정
+
+    beforeEach(async () => {
+      const uncleared = await request(app.getHttpServer()).get('/admin');
+      await Promise.all(
+        uncleared.body.map(async (admin) => {
+          return request(app.getHttpServer()).delete(`/admin/${admin.id}`);
+        }),
+      );
+    });
+
+    /**
+     * 데이터 조회
+     */
+    it('/admin (GET)', async () => {
+      const data = await request(app.getHttpServer()).get('/admin').expect(200);
+      expect(data).toBeTruthy();
+      expect(data.body.length).toBe(0);
+      return data;
+    });
+
+    const newAdmin = {
+      email: 'mion@gmail.com',
+      password: '1111',
+      role: {
+        role_code: 20,
+      },
+    };
+
+    /**
+     * 테스트 데이터 등록
+     */
+    it('/admin (POST)', async () => {
+      const data = await request(app.getHttpServer())
+        .post('/admin')
+        .send(newAdmin)
+        .expect(201);
+      expect(data).toBeTruthy();
+      expect(data.header).toHaveProperty('location');
+      expect(data.header.location).toStrictEqual(expect.any(String));
+
+      const savedData = await request(app.getHttpServer())
+        .get(`/admin/${data.header.location}`)
+        .expect(200);
+      expect(savedData.body).toBeTruthy();
+      expect(savedData.body.role.role_code).toBe(20);
+      console.log(savedData.body);
+      return data;
+    });
+
+    /**
+     * 테스트 데이터 등록(충돌) 테스트
+     */
+    it('/admin (POST)', async () => {
+      let data = await request(app.getHttpServer())
+        .post('/admin')
+        .send(newAdmin)
+        .expect(201);
+
+      data = await request(app.getHttpServer())
+        .post('/admin')
+        .send(newAdmin)
+        .expect(409);
+      expect(data).toBeTruthy();
+      return data;
+    });
   });
 });
--- apps/session-api/test/app.e2e-spec.ts
+++ apps/session-api/test/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { SessionApiModule } from './../src/session-api.module';
+import request from 'supertest';
+import {MainModule} from "../src/main.module";
 
 describe('SessionApiController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [SessionApiModule],
+      imports: [MainModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
